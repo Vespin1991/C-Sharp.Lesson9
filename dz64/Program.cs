0b635fc4d3993ed4e4cd66e928fce63deabda0bd
Aleksandr <Vespin1991@bk.ru>
2022-10-02 16:30:49 +0300

    dz64

---
+++ dz64/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//  Выполнить с помощью рекурсии.
+
+void Spisok (int N)
+{
+    if (N > 0)
+    {
+        Console.Write(N);
+        Spisok(N - 1);
+    }
+}
+Console.WriteLine("Введите число :");
+int N = Convert.ToInt32(Console.ReadLine());
+Spisok(N);
