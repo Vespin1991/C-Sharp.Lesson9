b2b5fa878fa617c05ee6a4cdc66bff7b9d8c35eb
Aleksandr <Vespin1991@bk.ru>
2022-10-02 16:38:38 +0300

    dz666/2

--- dz666/Program.cs
+++ dz666/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Summa (int M, int N)
+{
+    if ( M == N) return M;
+    else return M + Summa(M+1,N);
+}
+Console.WriteLine("Введите число M :");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N :");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"сумма элементов в промежутке от M до N = {Summa(M,N)}");
